e31d666b3896f17a20e4ad09f81e7fcc0d52a2c6
MacKenzie Richards <mrrichards@muhlenberg.edu>
2022-08-26 12:52:59 -0400

    first commit

---
+++ src/main/java/com/capgemini/BookDao.java
@@ -0,0 +1,19 @@
+package com.capgemini;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository
+public class BookDao {
+    public List<Book> getBooks() {
+        return Stream.of(
+                        new Book(101, "Crime and Punishment", 10, 100),
+                        new Book(58, "Gravity's Rainbow", 4, 60),
+                        new Book(205, "Catcher in the Rye", 1, 5),
+                        new Book(809, "Learning Java", 2, 100))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/capgemini/BookService.java
@@ -0,0 +1,32 @@
+package com.capgemini;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/books")
+public class BookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @GetMapping
+    public List<Book> fetchBooks() {
+        return bookDao.getBooks().stream().
+                sorted(Comparator.comparing(Book::getPrice)).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookService.class, args);
+    }
+
+}
